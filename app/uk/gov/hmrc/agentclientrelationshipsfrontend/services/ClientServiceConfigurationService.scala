c7b99dd8cb8f8f892d6c51987904be290f0aec9c
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2024-10-23 14:08:46 +0100

    wip

--- app/uk/gov/hmrc/agentclientrelationshipsfrontend/services/ClientServiceConfigurationService.scala
+++ app/uk/gov/hmrc/agentclientrelationshipsfrontend/services/ClientServiceConfigurationService.scala
@@ -33,18 +33,12 @@ class ClientServiceConfigurationService @Inject() {
     "HMRC-MTD-IT" -> ServiceData(
       serviceName = "HMRC-MTD-IT",
       clientTypes = Set("personal"),
-      clientDetails = Seq(
+      clientId = Seq(
         FieldConfiguration(
           name = "nino",
           regex = "[[A-Z]&&[^DFIQUV]][[A-Z]&&[^DFIQUVO]] ?\\d{2} ?\\d{2} ?\\d{2} ?[A-D]{1}",
           inputType = "text",
           width = 10
-        ),
-        FieldConfiguration(
-          name = "postcode",
-          regex = "^[A-Z]{1,2}[0-9][0-9A-Z]?\\s?[0-9][A-Z]{2}$|BFPO\\s?[0-9]{1,5}$",
-          inputType = "text",
-          width = 20
         )
       )
     ),
