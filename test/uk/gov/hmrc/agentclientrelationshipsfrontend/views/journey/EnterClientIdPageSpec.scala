1490fdb8d6bac5fb7dbd5e282f2029d50c4f9a6d
raw1697 <romel.w@hotmail.co.uk>
2024-10-31 15:53:50 +0000

    APB-8854 Spec WIP

--- test/uk/gov/hmrc/agentclientrelationshipsfrontend/views/journey/EnterClientIdPageSpec.scala
+++ test/uk/gov/hmrc/agentclientrelationshipsfrontend/views/journey/EnterClientIdPageSpec.scala
@@ -20,7 +20,8 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import play.api.data.Form
 import play.twirl.api.HtmlFormat
-import uk.gov.hmrc.agentclientrelationshipsfrontend.models.forms.journey.SelectFromOptionsForm
+import uk.gov.hmrc.agentclientrelationshipsfrontend.models.common.FieldConfiguration
+import uk.gov.hmrc.agentclientrelationshipsfrontend.models.forms.journey.EnterClientIdForm
 import uk.gov.hmrc.agentclientrelationshipsfrontend.models.journey.*
 import uk.gov.hmrc.agentclientrelationshipsfrontend.support.ViewSpecSupport
 import uk.gov.hmrc.agentclientrelationshipsfrontend.views.html.journey.EnterClientIdPage
@@ -29,44 +30,75 @@ class EnterClientIdPageSpec extends ViewSpecSupport {
 
   val viewTemplate: EnterClientIdPage = app.injector.instanceOf[EnterClientIdPage]
 
+  val ninoField: FieldConfiguration = FieldConfiguration(
+    name = "nino",
+    regex = "[[A-Z]&&[^DFIQUV]][[A-Z]&&[^DFIQUVO]] ?\\d{2} ?\\d{2} ?\\d{2} ?[A-D]{1}",
+    inputType = "text",
+    width = 10
+  )
+  
   object Expected {
 
     val FormError = "Select what you want the client to authorise you to do"
 
 
-    val clientIdNinoLabel = "What is your client’s National Insurance number?"
-    val clientIdNinoHint = "For example, QQ 12 34 56 C"
-    val clientIdNinoErrorInvalid = "National Insurance number must be 2 letters, 6 numbers, then A, B, C or D, like QQ 12 34 56 C"
-    val clientIdNinoErrorRequired = "Enter your client’s National Insurance number"
-
-    val clientIdVrnLabel = "What is your client’s VAT registration number?"
-    val clientIdVrnHint = "This is 9 numbers, for example, 123456789"
-    val clientIdVrnErrorInvalid = "VAT registration number must be 9 numbers clientId.vrn.error.required = Enter your client’s VAT registration number"
-
-    val clientIdUtrLabel = "What is your client’s Unique Taxpayer Reference (UTR)?"
-    val clientIdUtrHint = "Enter the last 10 digits only.For example, 1234567890"
-    val clientIdUtrErrorInvalid = "Enter the Unique Taxpayer Reference(UTR) or Unique Reference Number(URN) in the correct format"
-    val clientIdUtrErrorRequired = "Enter your client’s Unique Taxpayer Reference (UTR) or Unique Reference Number(URN)"
-
-    val clientIdCgtRefLabel = "What is your client’s Capital Gains Tax account reference?"
-    val clientIdCgtRefHint = "This is 15 characters, for example XYCGTP123456789. Your client receivedthis when they created their account."
-    val clientIdCgtRefErrorInvalid = "Enter your client’s Capital Gains Tax account reference"
-    val clientIdCgtRefErrorRequired = "Enter your client’s Capital Gains Tax account reference in the correct format"
-
-    val clientIdPptRefLabel = "What is your client’s Plastic Packaging Tax reference"
-    val clientIdPptRefHint = "This is 15 characters, for example XMPPT0000000001. Your client received this when they registered for Plastic Packaging Tax."
-    val clientIdPptRefErrorInvalid = "Enter your client’s Plastic Packaging Tax reference in the correct format"
-    val clientIdPptRefErrorRequired = "Enter your client’s Plastic Packaging Tax reference"
-
-    val clientIdCbcIdLabel = "What is your client’s Country-by-country ID"
-    val clientIdCbcIdHint = "This is the email address we currently hold for your client. If they have 2 contacts, enter the email address for the first contact."
-    val clientIdCbcIdErrorInvalid = "Enter your client’s country-by-country ID must start with an ’X’ followed by a letter, then ’CBC’ and then 10 numbers"
-    val clientIdCbcIdErrorRequired = "Enter your client’s country-by-country ID"
-
-    val clientIdPillar2Label = "What is your client’s Pillar 2 top -up taxes ID"
-    val clientIdPillar2Hint = "This is 15 characters, for example, XAPLR0000999999.The current filing member can find it on their Report Pillar2 top -up taxes homepage."
-    val clientIdPillar2ErrorInvalid = "Enter a valid Pillar 2 top -up taxes registration date"
-    val clientIdPillar2ErrorRequired = "Enter your client’s date of registration to report Pillar2 top -up taxes"
+    object nino {
+      val label = "What is your client’s National Insurance number?"
+      val hint = "For example, QQ 12 34 56 C"
+      val errorInvalid = "National Insurance number must be 2 letters, 6 numbers, then A, B, C or D, like QQ 12 34 56 C"
+      val errorRequired = "Enter your client’s National Insurance number"
+      val authorisationRequestTitle = s"$label - Ask a client to authorise you - GOV.UK"
+      val cancelAuthorisationTitle = s"$label - Cancel a client’s authorisation - GOV.UK"
+      val field: FieldConfiguration = ninoField
+    }
+    object vrn {
+      val label = "What is your client’s VAT registration number?"
+      val hint = "This is 9 numbers, for example, 123456789"
+      val errorInvalid = "VAT registration number must be 9 numbers " 
+      val errorRequired = "Enter your client’s VAT registration number"
+      val authorisationRequestTitle = s"$label - Ask a client to authorise you - GOV.UK"
+      val cancelAuthorisationTitle = s"$label - Cancel a client’s authorisation - GOV.UK"
+    }
+   object utr {
+     val label = "What is your client’s Unique Taxpayer Reference (UTR)?"
+     val hint = "Enter the last 10 digits only.For example, 1234567890"
+     val errorInvalid = "Enter the Unique Taxpayer Reference(UTR) or Unique Reference Number(URN) in the correct format"
+     val errorRequired = "Enter your client’s Unique Taxpayer Reference (UTR) or Unique Reference Number(URN)"
+     val authorisationRequestTitle = s"$label - Ask a client to authorise you - GOV.UK"
+     val cancelAuthorisationTitle =  s"$label - Cancel a client’s authorisation - GOV.UK"
+   }
+   object cgtRef {
+     val label = "What is your client’s Capital Gains Tax account reference?"
+     val hint = "This is 15 characters, for example XYCGTP123456789. Your client receivedthis when they created their account."
+     val errorInvalid = "Enter your client’s Capital Gains Tax account reference"
+     val errorRequired = "Enter your client’s Capital Gains Tax account reference in the correct format"
+     val authorisationRequestTitle = s"$label - Ask a client to authorise you - GOV.UK"
+     val cancelAuthorisationTitle = s"$label - Cancel a client’s authorisation - GOV.UK"
+   }
+    object pptRef {
+      val label = "What is your client’s Plastic Packaging Tax reference"
+      val hint = "This is 15 characters, for example XMPPT0000000001. Your client received this when they registered for Plastic Packaging Tax."
+      val errorInvalid = "Enter your client’s Plastic Packaging Tax reference in the correct format"
+      val errorRequired = "Enter your client’s Plastic Packaging Tax reference"
+      val authorisationRequestTitle = s"$label - Ask a client to authorise you - GOV.UK"
+      val cancelAuthorisationTitle = s"$label - Cancel a client’s authorisation - GOV.UK"
+    }
+    object cbcId {
+      val label = "What is your client’s Country-by-country ID"
+      val hint = "This is the email address we currently hold for your client. If they have 2 contacts, enter the email address for the first contact."
+      val errorInvalid = "Enter your client’s country-by-country ID must start with an ’X’ followed by a letter, then ’CBC’ and then 10 numbers"
+      val errorRequired = "Enter your client’s country-by-country ID"
+      val authorisationRequestTitle = s"$label - Ask a client to authorise you - GOV.UK"
+      val cancelAuthorisationTitle = s"$label - Cancel a client’s authorisation - GOV.UK"
+    }
+    object plrId {
+      val label = "What is your client’s Pillar 2 top -up taxes ID"
+      val hint = "This is 15 characters, for example, XAPLR0000999999.The current filing member can find it on their Report Pillar2 top -up taxes homepage."
+      val errorInvalid = "Enter a valid Pillar 2 top -up taxes registration date"
+      val errorRequired = "Enter your client’s date of registration to report Pillar2 top -up taxes"
+      val authorisationRequestTitle = s"$label - Ask a client to authorise you - GOV.UK"
+      val cancelAuthorisationTitle =  s"$label - Cancel a client’s authorisation - GOV.UK"
+    }
 
     val buttonContent = "Continue"
   }
@@ -74,11 +106,11 @@ class EnterClientIdPageSpec extends ViewSpecSupport {
   "EnterClientId for authorisation request view" should {
     implicit val journeyRequest: AgentJourneyRequest[?] = new AgentJourneyRequest("", Journey(journeyType = JourneyType.AuthorisationRequest), request)
 
-    val form: Form[String] = SelectFromOptionsForm.form("clientDetailField", options, "authorisation-request")
-    val view: HtmlFormat.Appendable = viewTemplate(form, options)
+    val form: Form[String] = EnterClientIdForm.form(ninoField, "authorisation-request")
+    val view: HtmlFormat.Appendable = viewTemplate(form, ninoField)
     val doc: Document = Jsoup.parse(view.body)
     "have the right title" in {
-      doc.title() shouldBe Expected.authorisationRequestTitle
+      doc.title() shouldBe Expected.nino.authorisationRequestTitle
     }
     "have a language switcher" in {
       doc.hasLanguageSwitch shouldBe true
@@ -94,9 +126,9 @@ class EnterClientIdPageSpec extends ViewSpecSupport {
     
     "render error for the correct journey" in {
       val formWithErrors = form.bind(Map.empty)
-      val viewWithErrors: HtmlFormat.Appendable = viewTemplate(formWithErrors, options)
+      val viewWithErrors: HtmlFormat.Appendable = viewTemplate(formWithErrors, ninoField)
       val docWithErrors: Document = Jsoup.parse(viewWithErrors.body)
-      docWithErrors.mainContent.extractText(errorSummaryList, 1).value shouldBe "Select the type of client you need authorisation from"
+      docWithErrors.mainContent.extractText(errorSummaryList, 1).value shouldBe Expected.clientIdNinoErrorRequired
     }
   }
 }
