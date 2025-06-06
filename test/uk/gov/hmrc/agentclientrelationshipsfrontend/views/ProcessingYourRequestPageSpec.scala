2817c96e2e7d05c57a3f0f98afff41b9d58ee3b2
Valentin Lukovski <47025947+VLukovski@users.noreply.github.com>
2025-06-06 11:56:23 +0100

    wip

--- test/uk/gov/hmrc/agentclientrelationshipsfrontend/views/ProcessingYourRequestPageSpec.scala
+++ test/uk/gov/hmrc/agentclientrelationshipsfrontend/views/ProcessingYourRequestPageSpec.scala
@@ -19,8 +19,9 @@ package uk.gov.hmrc.agentclientrelationshipsfrontend.views
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import play.twirl.api.HtmlFormat
+import uk.gov.hmrc.agentclientrelationshipsfrontend.models.journey.AgentJourneyType.AgentCancelAuthorisation
 import uk.gov.hmrc.agentclientrelationshipsfrontend.support.ViewSpecSupport
-import uk.gov.hmrc.agentclientrelationshipsfrontend.views.html.{ProcessingYourRequestPage, UserTimedOut}
+import uk.gov.hmrc.agentclientrelationshipsfrontend.views.html.ProcessingYourRequestPage
 
 class ProcessingYourRequestPageSpec extends ViewSpecSupport {
 
@@ -28,11 +29,19 @@ class ProcessingYourRequestPageSpec extends ViewSpecSupport {
 
   val testUrl = "/url"
 
-  object Expected {
+  object ExpectedClient {
     val title = "We’re processing your request - Appoint someone to deal with HMRC for you - GOV.UK"
     val heading = "We’re processing your request"
     val para1 = "You reached this page because you clicked twice to submit a request."
-    val para2  = "To see the confirmation screen, wait a few seconds and then select ‘continue’."
+    val para2 = "To see the confirmation screen, wait a few seconds and then select ‘continue’."
+    val button = "Continue"
+  }
+
+  object ExpectedAgent {
+    val title = "We’re processing your request - Cancel a client’s authorisation - GOV.UK"
+    val heading = "We’re processing your request"
+    val para1 = "You reached this page because you clicked twice to submit a request."
+    val para2 = "To see the confirmation screen, wait a few seconds and then select ‘continue’."
     val button = "Continue"
   }
 
@@ -41,20 +50,40 @@ class ProcessingYourRequestPageSpec extends ViewSpecSupport {
       val view: HtmlFormat.Appendable = viewTemplate(testUrl, isAgent = false)
       val doc: Document = Jsoup.parse(view.body)
       "have the right title" in {
-        doc.title() shouldBe Expected.title
+        doc.title() shouldBe ExpectedClient.title
       }
       "have a language switcher" in {
         doc.hasLanguageSwitch shouldBe true
       }
       "have the right heading" in {
-        doc.mainContent.extractText(h1, 1).value shouldBe Expected.heading
+        doc.mainContent.extractText(h1, 1).value shouldBe ExpectedClient.heading
       }
       "have the right paras" in {
-        doc.mainContent.extractText(p, 1).value shouldBe Expected.para1
-        doc.mainContent.extractText(p, 2).value shouldBe Expected.para2
+        doc.mainContent.extractText(p, 1).value shouldBe ExpectedClient.para1
+        doc.mainContent.extractText(p, 2).value shouldBe ExpectedClient.para2
       }
       "have a link button" in {
-        doc.mainContent.extractLinkButton(1).value shouldBe TestLink(Expected.button, testUrl)
+        doc.mainContent.extractLinkButton(1).value shouldBe TestLink(ExpectedClient.button, testUrl)
+      }
+    }
+    "viewed as an agent" should {
+      val view: HtmlFormat.Appendable = viewTemplate(testUrl, isAgent = true, optAgentJourney = Some(AgentCancelAuthorisation))
+      val doc: Document = Jsoup.parse(view.body)
+      "have the right title" in {
+        doc.title() shouldBe ExpectedAgent.title
+      }
+      "have a language switcher" in {
+        doc.hasLanguageSwitch shouldBe true
+      }
+      "have the right heading" in {
+        doc.mainContent.extractText(h1, 1).value shouldBe ExpectedAgent.heading
+      }
+      "have the right paras" in {
+        doc.mainContent.extractText(p, 1).value shouldBe ExpectedAgent.para1
+        doc.mainContent.extractText(p, 2).value shouldBe ExpectedAgent.para2
+      }
+      "have a link button" in {
+        doc.mainContent.extractLinkButton(1).value shouldBe TestLink(ExpectedAgent.button, testUrl)
       }
     }
   }
