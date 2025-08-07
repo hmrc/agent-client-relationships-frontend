d623ded5e05639086c895e24e7abbfb3d03b2b7f
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2025-08-07 13:43:45 +0100

    APB-9324 point to ASA statement (#130)
    * APB-9324 point to ASA statement
    
    * APB-9324 dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,10 +7,10 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.7.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.8.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.3.0",
-    "uk.gov.hmrc"       %% "crypto-json-play-30"                    % "8.2.0"
+    "uk.gov.hmrc"       %% "crypto-json-play-30"                    % "8.3.0"
   )
 
   val test: Seq[ModuleID] = Seq(
