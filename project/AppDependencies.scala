71068ff9745eb8f118b2a4beb91ce3e8c8b13809
kaizar12 <10980326+kaizar12@users.noreply.github.com>
2025-12-24 09:58:41 +0000

    APB-10448 upgrade dependency as per platops-pr-bot suggestion

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.22.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.24.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.4.0",
     "uk.gov.hmrc"       %% "crypto-json-play-30"                    % "8.4.0"
