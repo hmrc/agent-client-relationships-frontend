23cdced83d101b21d9b97173e9df6779216d3c9f
Mohammad Abdulhadi <mohammad.abdulhadi@testdrivensolutions.co.uk>
2026-03-09 16:34:28 +0000

    Upgraded all dependencies to the latest version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,20 +3,21 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "9.19.0"
-  private val hmrcMongoVersion = "2.11.0"
+  private val hmrcMongoVersion = "2.12.0"
+  private val playFrameworkVersion = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.25.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.4.0",
-    "uk.gov.hmrc"       %% "crypto-json-play-30"                    % "8.4.0"
+    "uk.gov.hmrc"       %% s"bootstrap-frontend-$playFrameworkVersion"             % bootstrapVersion,
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playFrameworkVersion"             % "12.32.0",
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playFrameworkVersion"                     % hmrcMongoVersion,
+    "uk.gov.hmrc"       %% s"play-conditional-form-mapping-$playFrameworkVersion"  % "3.5.0",
+    "uk.gov.hmrc"       %% s"crypto-json-$playFrameworkVersion"                    % "8.4.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-30"                 % bootstrapVersion  % Test,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30"                % hmrcMongoVersion  % Test,
-    "org.jsoup"         % "jsoup"                                   % "1.19.1"          % Test
+    "uk.gov.hmrc"       %% s"bootstrap-test-$playFrameworkVersion"                 % bootstrapVersion  % Test,
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test-$playFrameworkVersion"                % hmrcMongoVersion  % Test,
+    "org.jsoup"         % "jsoup"                                                  % "1.22.1"          % Test
   )
 
 }
