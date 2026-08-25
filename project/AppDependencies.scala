8fca33c4284ed054b9ab8561307fe346c1f9c654
Drew Miley <13115186+drewmiley@users.noreply.github.com>
2026-08-25 11:07:35 +0100

    APB-12601: Upgrades play-frontend-hmrc to 13.11.0 and hmrc-mongo to 2.13.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,12 +4,12 @@ object AppDependencies {
 
   private val playVer = "play-30"
   private val bootstrapVer = "9.19.0"
-  private val mongoVer = "2.12.0"
+  private val mongoVer = "2.13.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVer"            % bootstrapVer,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVer"                    % mongoVer,
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVer"            % "13.1.0",
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVer"            % "13.11.0",
     "uk.gov.hmrc"       %% s"play-conditional-form-mapping-$playVer" % "3.5.0",
     "uk.gov.hmrc"       %% s"crypto-json-$playVer"                   % "8.4.0"
   )
